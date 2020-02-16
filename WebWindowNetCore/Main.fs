ede5188c7cad9dbdc8e8dfa47af41b8b58577856
Uwe Riegel <uriegel@hotmail.de>
2020-02-16 09:56:52 +0100

    DllName

--- WebWindowNetCore/Main.fs
+++ WebWindowNetCore/Main.fs
@@ -5,7 +5,7 @@ open System.Runtime.InteropServices
 module WebWindow = 
     
     [<Literal>]
-    let private DllName = "libminimal.so.1.0.0"
+    let private DllName = "libwebwindow.so.1.0.0"
 
     [<DllImport(DllName, EntryPoint = "run_web_window", CallingConvention = CallingConvention.Cdecl)>] 
     extern void runWebWindow (string url)
