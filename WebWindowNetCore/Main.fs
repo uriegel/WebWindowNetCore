3d8b5a10537648f6d8e81d3da2226e5d5f6121a0
Uwe Riegel <uriegel@hotmail.de>
2020-02-22 15:13:00 +0100

    TODOs

--- WebWindowNetCore/Main.fs
+++ WebWindowNetCore/Main.fs
@@ -36,6 +36,11 @@ let initialize (configuration: Configuration) =
     let c = NativeConfiguration(title = configuration.title, url = configuration.url)
     NativeMethods.Initialize c
     
+    // TODO: Developer Tools
     // TODO: To debug: Chrome: localhost:8888
+    // TODO: Save and Resote Window Settings
+    // TODO: Icon
+    // TODO: Full screen
+    // TODO: Menu
 
 let execute = NativeMethods.Execute 
