6af46e1286ceeb5e8ff1df72042558a82469dad3
Uwe Riegel <uriegel@hotmail.de>
2022-02-27 17:00:24 +0100

    Now Gtk Webview is displaying web site

--- Native/WebWindowNetCore.Linux/Program.cs
+++ Native/WebWindowNetCore.Linux/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace WebWindowNetCore;
+using GtkDotNet;
 public static class Program
 {
     public static void Execute()
     {
-        Console.WriteLine("Na das wars von Linux");
-        Console.ReadLine();
+        var app = new Application("de.uriegel.test");
+        var ret = app.Run(() => 
+        {
+            app.EnableSynchronizationContext();
+            var webView = new WebView();
+            var window = new Window();
+            window.Add(webView);
+            webView.LoadUri("https://www.microsoft.com");
+            webView.Settings.EnableDeveloperExtras = true;
+            app.AddWindow(window);
+            window.SetTitle("Web View 😎😎👌");
+            window.SetDefaultSize(800, 600);
+            window.ShowAll();
+        });
     }
 }
