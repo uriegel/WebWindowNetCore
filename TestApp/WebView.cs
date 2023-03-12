2a31476ef1aabc55e029b4861b04d7251f098bdf
Uwe Riegel <uriegel@hotmail.de>
2023-03-12 11:22:14 +0100

    loading global script

--- TestApp/WebView.cs
+++ TestApp/WebView.cs
@@ -54,15 +54,14 @@ public class WebView : WebWindowNetCore.WebView
             {
                 var w = settings?.Width;
                 var h = settings?.Height;
+                var showDevTools = settings?.DevTools == true;
+                var withFetch = (settings?.HttpSettings?.RequestDelegates?.Length ?? 0) > 0;
                 webView.LoadChanged += (s, e) =>
                 {
                     if (e.LoadEvent == WebKitLoadEvent.WEBKIT_LOAD_COMMITTED)
+                    {
                         webView.RunJavascript(
                         """ 
-                            function webViewShowDevTools() {
-                                alert(JSON.stringify({action: 1}))
-                            }
-
                             const bounds = JSON.parse(localStorage.getItem('window-bounds') || '{}')
                             const isMaximized = localStorage.getItem('isMaximized')
                             if (bounds.width && bounds.height)
@@ -70,6 +69,29 @@ public class WebView : WebWindowNetCore.WebView
                             else
                                 alert(JSON.stringify({action: 2}))
                         """);
+                        if (showDevTools == true)
+                            webView.RunJavascript(
+                            """ 
+                                function webViewShowDevTools() {
+                                    alert(JSON.stringify({action: 1}))
+                                }
+                            """);
+                        if (withFetch)
+                            webView.RunJavascript(
+                            """ 
+                                async function webViewRequest(method, input) {
+
+                                    const msg = {
+                                        method: 'POST',
+                                        headers: { 'Content-Type': 'application/json' },
+                                        body: JSON.stringify(input)
+                                    }
+
+                                    const response = await fetch(`/request/${method}`, msg) 
+                                    return await response.json() 
+                                }
+                            """);
+                    }
                 };
 
                 window.Configure += (s, e) =>
