6bcbe6da86ef5a36cb8944b537bd51f34632ccd5
Uwe Riegel <uwe.riegel@caseris.de>
2024-08-07 15:13:01 +0200

    beta 2

--- TestAppWindows/Program.cs
+++ TestAppWindows/Program.cs
@@ -1,124 +1,127 @@
-﻿#if Windows
+﻿
+using WebWindowNetCore.Base;
+Test.Run();
+// #if Windows
 
-using AspNetExtensions;
-using CsTools.Extensions;
-using WebWindowNetCore;
+// using AspNetExtensions;
+// using CsTools.Extensions;
+// using WebWindowNetCore;
 
-var sseEventSource = WebView.CreateEventSource<Event>();
-StartEvents(sseEventSource.Send);
+// var sseEventSource = WebView.CreateEventSource<Event>();
+// StartEvents(sseEventSource.Send);
 
-WebView
-    .Create()
-    .InitialBounds(800, 600)
-    .Title("WebView Test")
-    .ResourceIcon("icon")
-    .SaveBounds()
-    //.DebugUrl("http://127.0.0.1:20000")
-    //.Url($"file://{Directory.GetCurrentDirectory()}/webroot/index.html")
-    .ConfigureHttp(http => http
-        .ResourceWebroot("webroot", "/web")
-        .UseSse("sse/test", sseEventSource)
-        .UseReverseProxy("127.0.0.1", "", "http://localhost:5137")
+// WebView
+//     .Create()
+//     .InitialBounds(800, 600)
+//     .Title("WebView Test")
+//     .ResourceIcon("icon")
+//     .SaveBounds()
+//     //.DebugUrl("http://127.0.0.1:20000")
+//     //.Url($"file://{Directory.GetCurrentDirectory()}/webroot/index.html")
+//     .ConfigureHttp(http => http
+//         .ResourceWebroot("webroot", "/web")
+//         .UseSse("sse/test", sseEventSource)
+//         .UseReverseProxy("127.0.0.1", "", "http://localhost:5137")
 
-        .MapGet("video", context => 
-            context
-                .SideEffect(c => Console.WriteLine("Range request"))
-            .StreamRangeFile(@"C:\Users\uwe\Documents\Hafenrundfahrt.mp4"))
+//         .MapGet("video", context => 
+//             context
+//                 .SideEffect(c => Console.WriteLine("Range request"))
+//             .StreamRangeFile(@"C:\Users\uwe\Documents\Hafenrundfahrt.mp4"))
 
-        .Build())
-#if DEBUG
-    .DebuggingEnabled()
-#endif
-    .Build()
-    .Run();    
+//         .Build())
+// #if DEBUG
+//     .DebuggingEnabled()
+// #endif
+//     .Build()
+//     .Run();    
 
-void StartEvents(Action<Event> onChanged)   
-{
-    var counter = 0;
-    new Thread(_ =>
-        {
-            while (true)
-            {
-                Thread.Sleep(5000);
-                onChanged(new($"Ein Event {counter++}"));
-           }
-        })
-        {
-            IsBackground = true
-        }.Start();   
-}
+// void StartEvents(Action<Event> onChanged)   
+// {
+//     var counter = 0;
+//     new Thread(_ =>
+//         {
+//             while (true)
+//             {
+//                 Thread.Sleep(5000);
+//                 onChanged(new($"Ein Event {counter++}"));
+//            }
+//         })
+//         {
+//             IsBackground = true
+//         }.Start();   
+// }
 
-record Event(string Content);
-// TODO take transparent drag source window from test project
+// record Event(string Content);
+// // TODO take transparent drag source window from test project
 
-/*
-using System.Runtime.InteropServices;
+// /*
+// using System.Runtime.InteropServices;
 
 
-*/
-/*
-Overlay
+// */
+// /*
+// Overlay
 
-namespace WebView2Form;
-public partial class Overlay : Form
-{
-    public Overlay(Form parent) 
-    {
-        InitializeComponent();
-    }
-}
-using System.Runtime.InteropServices;
+// namespace WebView2Form;
+// public partial class Overlay : Form
+// {
+//     public Overlay(Form parent) 
+//     {
+//         InitializeComponent();
+//     }
+// }
+// using System.Runtime.InteropServices;
 
-namespace WebView2Form;
+// namespace WebView2Form;
 
-namespace WebView2Form;
+// namespace WebView2Form;
 
-partial class Overlay
-{
-    /// <summary>
-    /// Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
+// partial class Overlay
+// {
+//     /// <summary>
+//     /// Required designer variable.
+//     /// </summary>
+//     private System.ComponentModel.IContainer components = null;
 
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
-    {
-        if (disposing && (components != null))
-        {
-            components.Dispose();
-        }
-        base.Dispose(disposing);
-    }
+//     /// <summary>
+//     /// Clean up any resources being used.
+//     /// </summary>
+//     /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+//     protected override void Dispose(bool disposing)
+//     {
+//         if (disposing && (components != null))
+//         {
+//             components.Dispose();
+//         }
+//         base.Dispose(disposing);
+//     }
 
-    #region Windows Form Designer generated code
+//     #region Windows Form Designer generated code
 
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-            this.SuspendLayout();
-            // 
-            // Overlay
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Overlay";
-            this.Opacity = 0.3D;
-            this.Text = "Overlay";
-            this.ResumeLayout(false);
+//     /// <summary>
+//     /// Required method for Designer support - do not modify
+//     /// the contents of this method with the code editor.
+//     /// </summary>
+//     private void InitializeComponent()
+//     {
+//             this.SuspendLayout();
+//             // 
+//             // Overlay
+//             // 
+//             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+//             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+//             this.ClientSize = new System.Drawing.Size(800, 450);
+//             this.Name = "Overlay";
+//             this.Opacity = 0.3D;
+//             this.Text = "Overlay";
+//             this.ResumeLayout(false);
 
-    }
+//     }
 
-    #endregion
-}*/
+//     #endregion
+// }*/
 
-#else
+// #else
 
-System.Console.WriteLine("Windows required");
-#endif
+// System.Console.WriteLine("Windows required");
+// #endif
--- TestAppWindows/WebWindowForm.cs
+++ TestAppWindows/WebWindowForm.cs
@@ -1,155 +1,155 @@
-#if Windows
+// #if Windows
 
-using CsTools.Extensions;
-using Microsoft.Web.WebView2.Core;
-using Microsoft.Web.WebView2.WinForms;
-using WebWindowNetCore.Data;
+// using CsTools.Extensions;
+// using Microsoft.Web.WebView2.Core;
+// using Microsoft.Web.WebView2.WinForms;
+// using WebWindowNetCore.Data;
 
-namespace WebWindowNetCore;
+// namespace WebWindowNetCore;
 
-public class WebWindowForm : Form
-{
-    public void Init(int width, int height, bool maximize)  
-    {
-        ClientSize = new System.Drawing.Size(width, height);
-        if (maximize)
-            WindowState = FormWindowState.Maximized;
+// public class WebWindowForm : Form
+// {
+//     public void Init(int width, int height, bool maximize)  
+//     {
+//         ClientSize = new System.Drawing.Size(width, height);
+//         if (maximize)
+//             WindowState = FormWindowState.Maximized;
         
-    }
+//     }
 
-    public void ShowDevtools()
-        => webView.CoreWebView2.OpenDevToolsWindow();
+//     public void ShowDevtools()
+//         => webView.CoreWebView2.OpenDevToolsWindow();
     
-    public WebWindowForm(WebViewSettings? settings, string appDataPath) 
-    {
-        webView = new WebView2();
-        ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
-        SuspendLayout();
-        // 
-        // webView
-        // 
-        this.webView.AllowExternalDrop = true;
-        this.webView.CreationProperties = null;
-        this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
-        this.webView.Location = new System.Drawing.Point(0, 0);
-        this.webView.Margin = new System.Windows.Forms.Padding(0);
-        this.webView.Name = "webView";
-        this.webView.Dock = DockStyle.Fill;
-        this.webView.TabIndex = 0;
-        this.webView.ZoomFactor = 1D;
-        // 
-        // Form1
-        // 
-        //this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+//     public WebWindowForm(WebViewSettings? settings, string appDataPath) 
+//     {
+//         webView = new WebView2();
+//         ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+//         SuspendLayout();
+//         // 
+//         // webView
+//         // 
+//         this.webView.AllowExternalDrop = true;
+//         this.webView.CreationProperties = null;
+//         this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+//         this.webView.Location = new System.Drawing.Point(0, 0);
+//         this.webView.Margin = new System.Windows.Forms.Padding(0);
+//         this.webView.Name = "webView";
+//         this.webView.Dock = DockStyle.Fill;
+//         this.webView.TabIndex = 0;
+//         this.webView.ZoomFactor = 1D;
+//         // 
+//         // Form1
+//         // 
+//         //this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
         
-        if (settings?.ResourceIcon != null)
-            this.Icon = new System.Drawing.Icon(Resources.Get(settings.ResourceIcon)!);
+//         if (settings?.ResourceIcon != null)
+//             this.Icon = new System.Drawing.Icon(Resources.Get(settings.ResourceIcon)!);
 
-        this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-        this.WindowState = FormWindowState.Minimized;
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+//         this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+//         this.WindowState = FormWindowState.Minimized;
+//         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         
-        this.Controls.Add(this.webView);
-        this.Name = "WebWindow";
+//         this.Controls.Add(this.webView);
+//         this.Name = "WebWindow";
 
-        this.Text = settings?.Title;
-        ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
-        this.ResumeLayout(false);
+//         this.Text = settings?.Title;
+//         ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+//         this.ResumeLayout(false);
 
-        this.Resize += async (s, e) =>
-        {
-            if (initialized && settings?.SaveBounds == true) {
-                if (this.WindowState != FormWindowState.Maximized)
-                    await webView.ExecuteScriptAsync(
-                        $$"""
-                            localStorage.setItem('window-bounds', JSON.stringify({width: {{Width}}, height: {{Height}}}))
-                            localStorage.setItem('isMaximized', false)
-                        """);
-                else
-                    await webView.ExecuteScriptAsync($"localStorage.setItem('isMaximized', true)");
-            }
-        };
+//         this.Resize += async (s, e) =>
+//         {
+//             if (initialized && settings?.SaveBounds == true) {
+//                 if (this.WindowState != FormWindowState.Maximized)
+//                     await webView.ExecuteScriptAsync(
+//                         $$"""
+//                             localStorage.setItem('window-bounds', JSON.stringify({width: {{Width}}, height: {{Height}}}))
+//                             localStorage.setItem('isMaximized', false)
+//                         """);
+//                 else
+//                     await webView.ExecuteScriptAsync($"localStorage.setItem('isMaximized', true)");
+//             }
+//         };
 
-        StartWebviewInit();
+//         StartWebviewInit();
 
-        async void StartWebviewInit()
-        {
-            var enf = await  CoreWebView2Environment.CreateAsync(null, appDataPath);
-            await webView.EnsureCoreWebView2Async(enf);
-            webView.CoreWebView2.AddHostObjectToScript("Callback", new Callback(this));
-            webView.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
-            webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+//         async void StartWebviewInit()
+//         {
+//             var enf = await  CoreWebView2Environment.CreateAsync(null, appDataPath);
+//             await webView.EnsureCoreWebView2Async(enf);
+//             webView.CoreWebView2.AddHostObjectToScript("Callback", new Callback(this));
+//             webView.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
+//             webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
             
-            webView.CoreWebView2.ContainsFullScreenElementChanged += (objs, args) =>
-            {
-                if (webView.CoreWebView2.ContainsFullScreenElement)
-                {
-                    this.TopMost = true;
-                    this.FormBorderStyle = FormBorderStyle.None;
-                    this.WindowState = FormWindowState.Maximized;
-                    Taskbar.Hide();
-                }
-                else
-                {
-                    this.TopMost = false;
-                    this.WindowState = FormWindowState.Normal;
-                    this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
-                    Taskbar.Show();
-                }
-            };
+//             webView.CoreWebView2.ContainsFullScreenElementChanged += (objs, args) =>
+//             {
+//                 if (webView.CoreWebView2.ContainsFullScreenElement)
+//                 {
+//                     this.TopMost = true;
+//                     this.FormBorderStyle = FormBorderStyle.None;
+//                     this.WindowState = FormWindowState.Maximized;
+//                     Taskbar.Hide();
+//                 }
+//                 else
+//                 {
+//                     this.TopMost = false;
+//                     this.WindowState = FormWindowState.Normal;
+//                     this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+//                     Taskbar.Show();
+//                 }
+//             };
                 
-            if (settings?.Url != null)
-                webView.Source = new System.Uri(settings?.Url ?? "");
-            if (settings?.HttpSettings?.WebrootUrl != null)
-            {
-#if DEBUG
-                var uri = string.IsNullOrEmpty(settings?.DebugUrl)
-                    ? $"http://localhost:{settings?.HttpSettings?.Port ?? 80}{settings?.HttpSettings?.WebrootUrl}/{settings?.HttpSettings?.DefaultHtml}"
-                    : settings?.DebugUrl!;
-#else
-                var uri = $"http://localhost:{settings?.HttpSettings?.Port ?? 80}{settings?.HttpSettings?.WebrootUrl}/{settings?.HttpSettings?.DefaultHtml}";
-#endif                
-                webView.Source = new System.Uri(uri);
-            }
+//             if (settings?.Url != null)
+//                 webView.Source = new System.Uri(settings?.Url ?? "");
+//             if (settings?.HttpSettings?.WebrootUrl != null)
+//             {
+// #if DEBUG
+//                 var uri = string.IsNullOrEmpty(settings?.DebugUrl)
+//                     ? $"http://localhost:{settings?.HttpSettings?.Port ?? 80}{settings?.HttpSettings?.WebrootUrl}/{settings?.HttpSettings?.DefaultHtml}"
+//                     : settings?.DebugUrl!;
+// #else
+//                 var uri = $"http://localhost:{settings?.HttpSettings?.Port ?? 80}{settings?.HttpSettings?.WebrootUrl}/{settings?.HttpSettings?.DefaultHtml}";
+// #endif                
+//                 webView.Source = new System.Uri(uri);
+//             }
 
-            WindowState = FormWindowState.Normal;
-            initialized = true;
-            await webView.ExecuteScriptAsync(
-                $$"""
-                    const callback = chrome.webview.hostObjects.Callback
-                    callback.Ready()
-                    const bounds = JSON.parse(localStorage.getItem('window-bounds') || '{}')
-                    const isMaximized = localStorage.getItem('isMaximized')
-                    callback.Init(bounds.width || {{settings?.Width ?? 800}}, bounds.height || {{settings?.Height ?? 600}}, isMaximized == 'true')
-                """);
-            if (settings?.DevTools == true)
-                await webView.ExecuteScriptAsync(
-                    """ 
-                        function webViewShowDevTools() {
-                            callback.ShowDevtools()
-                        }
-                    """);
-            if ((settings?.HttpSettings?.RequestDelegates?.Length ?? 0) > 0)
-                await webView.ExecuteScriptAsync(
-                    """ 
-                        async function webViewRequest(method, input) {
+//             WindowState = FormWindowState.Normal;
+//             initialized = true;
+//             await webView.ExecuteScriptAsync(
+//                 $$"""
+//                     const callback = chrome.webview.hostObjects.Callback
+//                     callback.Ready()
+//                     const bounds = JSON.parse(localStorage.getItem('window-bounds') || '{}')
+//                     const isMaximized = localStorage.getItem('isMaximized')
+//                     callback.Init(bounds.width || {{settings?.Width ?? 800}}, bounds.height || {{settings?.Height ?? 600}}, isMaximized == 'true')
+//                 """);
+//             if (settings?.DevTools == true)
+//                 await webView.ExecuteScriptAsync(
+//                     """ 
+//                         function webViewShowDevTools() {
+//                             callback.ShowDevtools()
+//                         }
+//                     """);
+//             if ((settings?.HttpSettings?.RequestDelegates?.Length ?? 0) > 0)
+//                 await webView.ExecuteScriptAsync(
+//                     """ 
+//                         async function webViewRequest(method, input) {
 
-                            const msg = {
-                                method: 'POST',
-                                headers: { 'Content-Type': 'application/json' },
-                                body: JSON.stringify(input)
-                            }
+//                             const msg = {
+//                                 method: 'POST',
+//                                 headers: { 'Content-Type': 'application/json' },
+//                                 body: JSON.stringify(input)
+//                             }
 
-                            const response = await fetch(`/request/${method}`, msg) 
-                            return await response.json() 
-                        }
-                    """);                
-        }
-    }
+//                             const response = await fetch(`/request/${method}`, msg) 
+//                             return await response.json() 
+//                         }
+//                     """);                
+//         }
+//     }
 
 
-    WebView2 webView;
-    bool initialized;
-}
+//     WebView2 webView;
+//     bool initialized;
+// }
 
-#endif
+// #endif
