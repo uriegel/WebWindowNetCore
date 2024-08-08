7fb3fa1cf20f06d6cbb6d8a07a00b702b8ed7106
Uwe Riegel <uriegel@hotmail.de>
2024-08-08 19:54:38 +0200

    webview now showing in windows

--- WebWindowNetCore/WebViewForm.fs
+++ WebWindowNetCore/WebViewForm.fs
@@ -1,16 +1,46 @@
 namespace WebWindowNetCore
 #if Windows
+open System
+open System.Drawing
 open System.Windows.Forms
+open Microsoft.Web.WebView2.Core
 open Microsoft.Web.WebView2.WinForms
+open System.Threading
 
-type WebViewForm() = 
+type WebViewForm(appDataPath: string) = 
     inherit Form()
 
     let webView = new WebView2()
     
     do 
-        (webView :> System.ComponentModel.ISupportInitialize).BeginInit()
+        (webView :> ComponentModel.ISupportInitialize).BeginInit()
+        webView.DefaultBackgroundColor <- Color.White
+        webView.Location <- Point (0, 0)
+        webView.Margin <- Padding 0
+        webView.Dock <- DockStyle.Fill
+        webView.TabIndex <- 0
+        webView.ZoomFactor <- 1
+
+        base.AutoScaleDimensions <- SizeF(8F, 20F)
+        base.AutoScaleMode <- AutoScaleMode.Font
+
         base.Text <- "Hallo"
+        base.Size <- Size (800, 600)
+        base.Controls.Add webView
+
+        (webView :> ComponentModel.ISupportInitialize).EndInit ()
+        base.ResumeLayout false
+
+        let startInContext (sync: SynchronizationContext) work = 
+            sync.Send((fun _ -> Async.StartImmediate(work)), null)
+
+        async {
+            let! enf = CoreWebView2Environment.CreateAsync(null, appDataPath, null) |> Async.AwaitTask
+            do! webView.EnsureCoreWebView2Async(enf) |> Async.AwaitTask
+            webView.Source <- Uri ("https://google.de")
+        } 
+        |> startInContext SynchronizationContext.Current
+
 
 #endif    
 
--- WebWindowNetCore/WebViewWindows.fs
+++ WebWindowNetCore/WebViewWindows.fs
@@ -1,12 +1,49 @@
 namespace WebWindowNetCore
 #if Windows
+open System.Diagnostics
 open System.Windows.Forms
+open System.Threading
+open System
+open System.Reflection
+
+open FSharpTools
+open System.IO
+open ClrWinApi
 
 type WebView() = 
     inherit WebViewBase()
+
+    let getWebViewLoader () =
+        let targetFileName = 
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+            |> Directory.attachSubPath (sprintf "de.uriegel.WebWindowNetCore\%s" <|Process.GetCurrentProcess().ProcessName)
+            |> Directory.ensureExists
+            |> Directory.attachSubPath "WebView2Loader.dll"
+        try 
+            use targetFile = File.Create targetFileName
+            Assembly
+                .GetExecutingAssembly()
+                .GetManifestResourceStream("binaries/webviewloader")
+                .CopyTo targetFile
+                |> ignore
+        with 
+        | _ -> ()
+        targetFileName
     
     override this.Run() =
-        let webForm = new WebViewForm()
+
+        Thread.CurrentThread.SetApartmentState ApartmentState.Unknown
+        Thread.CurrentThread.SetApartmentState ApartmentState.STA
+
+        Application.EnableVisualStyles ()
+        Application.SetCompatibleTextRenderingDefault false
+        Application.SetHighDpiMode HighDpiMode.PerMonitorV2 |> ignore
+
+        let loader = getWebViewLoader ()
+        let appDataPath = FileInfo(loader).DirectoryName
+        Api.LoadLibrary loader |> ignore
+
+        let webForm = new WebViewForm(appDataPath)
         Application.Run(webForm) 
         0
 
