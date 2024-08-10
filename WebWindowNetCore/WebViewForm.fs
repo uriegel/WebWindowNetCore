58b2379c99745a0311aa553fb43be3f0da974623
Uwe Riegel <uriegel@hotmail.de>
2024-08-11 00:04:52 +0200

    SaveBounds

--- WebWindowNetCore/WebViewForm.fs
+++ WebWindowNetCore/WebViewForm.fs
@@ -7,7 +7,7 @@ open Microsoft.Web.WebView2.Core
 open Microsoft.Web.WebView2.WinForms
 open FSharpTools
 
-type WebViewForm(appDataPath: string, settings: WebViewBase) = 
+type WebViewForm(appDataPath: string, settings: WebViewBase) as this = 
     inherit Form()
 
     let webView = new WebView2()
@@ -24,8 +24,18 @@ type WebViewForm(appDataPath: string, settings: WebViewBase) =
         base.AutoScaleDimensions <- SizeF(8F, 20F)
         base.AutoScaleMode <- AutoScaleMode.Font
 
+        let bounds = Bounds.retrieve settings.AppIdValue
+        if bounds.X.IsSome && bounds.Y.IsSome then
+            this.Location <- Point(bounds.X |> Option.defaultValue 0, bounds.Y |> Option.defaultValue 0)
+        this.Size <- Size(bounds.Width |> Option.defaultValue settings.WidthValue, bounds.Height |> Option.defaultValue settings.HeightValue)
+        this.WindowState <- if bounds.IsMaximized then FormWindowState.Maximized else FormWindowState.Normal
+
+        if settings.SaveBoundsValue then
+            this.FormClosing.Add(this.onClosing)
+
+        this.Load.Add(this.onLoad)
+
         base.Text <- settings.TitleValue
-        base.Size <- Size (settings.WidthValue, settings.HeightValue)
         base.Controls.Add webView
 
         (webView :> ComponentModel.ISupportInitialize).EndInit ()
@@ -38,7 +48,26 @@ type WebViewForm(appDataPath: string, settings: WebViewBase) =
         } 
         |> Async.StartWithCurrentContext 
 
-
+    member this.onLoad (_: EventArgs) =
+        let bounds = Bounds.retrieve settings.AppIdValue
+        if bounds.X.IsSome && bounds.Y.IsSome 
+                && Screen.AllScreens |> Seq.exists (fun s -> s.WorkingArea.IntersectsWith(Rectangle(
+                                                                                                            bounds.X |> Option.defaultValue 0, 
+                                                                                                            bounds.Y |> Option.defaultValue 0, 
+                                                                                                            this.Size.Width, 
+                                                                                                            this.Size.Height))) then
+            this.Location <- Point(bounds.X |> Option.defaultValue 0 , bounds.Y |> Option.defaultValue 0)
+            this.WindowState <- if bounds.IsMaximized then FormWindowState.Maximized else FormWindowState.Normal
+        ()
+    member this.onClosing (e: FormClosingEventArgs) =
+            { Bounds.retrieve settings.AppIdValue with 
+                X = if this.WindowState = FormWindowState.Maximized then Some this.RestoreBounds.Location.X else Some this.Location.X
+                Y = if this.WindowState = FormWindowState.Maximized then Some this.RestoreBounds.Location.Y else Some this.Location.Y
+                Width = if this.WindowState = FormWindowState.Maximized then Some this.RestoreBounds.Size.Width else Some this.Size.Width
+                Height = if this.WindowState = FormWindowState.Maximized then Some this.RestoreBounds.Size.Height else Some this.Size.Height
+                IsMaximized = this.WindowState = FormWindowState.Maximized }
+            |> Bounds.save settings.AppIdValue
+        
 #endif    
 
     
