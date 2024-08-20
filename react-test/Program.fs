2187c230b0d600c00ab2e66d354ed30bd7639487
Uwe Riegel <uriegel@hotmail.de>
2024-08-20 08:39:20 +0200

    react-test

---
+++ react-test/Program.fs
@@ -0,0 +1,28 @@
+﻿open WebWindowNetCore
+open System.IO
+open Requests
+
+type Input = { Text: string; Id: int }
+type Contact = { Name: string; Id: int }
+let getContact (text: Input) =
+    task { return { Name = "Uwe Riegel"; Id = 9865 } }  
+
+let onRequest (method: string) (input: Stream) =
+    task {   
+        return! 
+            match method with
+            | "cmd1" -> input |> GetInput |> getContact |> AsTask
+            | _ -> task { return obj() }
+    }
+
+WebView()
+    .AppId("de.uriegel.test")
+    .Title("React WebView")
+    .DebugUrl("http:localhost:5173")
+    .Url(sprintf "file://%s/webroot/index.html" (Directory.GetCurrentDirectory ()))
+    .SaveBounds()
+    .DefaultContextMenuDisabled()
+    .AddRequest<Input, Contact>("cmd1", getContact)
+    .DevTools()
+    .Run()
+    |> ignore
