302efb471b06cae5990115eeba8b75338ae86164
uriegel <uriegel@hotmail.de>
2025-02-13 08:08:01 +0100

    TestAppRes created

---
+++ TestAppRes/Program.cs
@@ -0,0 +1,18 @@
+﻿using WebWindowNetCore;
+
+WebView
+    .Create()
+    .AppId("de.uriegel.test")
+    .Title("Web Window Net Core from Resource 👍")
+    .InitialBounds(600, 800)
+    .SaveBounds()
+    .DefaultContextMenuDisabled()
+#if Windows
+    .ResourceIcon("icon")
+#endif
+    .DebugUrl("https://github.com/uriegel/WebWindowNetCore")
+    .Url("https://github.com")
+    .QueryString("?param1=123&param2=456")
+    .CanClose(() => true)
+    .Run();
+
