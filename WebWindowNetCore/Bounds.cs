2f025f89ae8b3f55aeeb8928ed4f97283f8b6bf1
Uwe Riegel <uriegel@hotmail.de>
2026-06-24 07:14:44 +0200

    Bounds refactured

--- WebWindowNetCore/Bounds.cs
+++ WebWindowNetCore/Bounds.cs
@@ -6,21 +6,19 @@ namespace WebWindowNetCore;
 record Bounds(int? X, int? Y, int? Width, int? Height, bool IsMaximized)
 {
     public static Bounds Retrieve(string id)
-        => Environment
-            .GetFolderPath(Environment.SpecialFolder.ApplicationData)
-            .AppendPath(id)
-            .SideEffect(d => d.EnsureDirectoryExists())
-            .AppendPath("bounds.json")
+        => GetPath(id)
             .ReadAllTextFromFilePath()
             .SideEffect(Console.WriteLine)
             ?.Deserialize<Bounds>(Json.Defaults) 
             ?? new(null, null, null, null, false);
 
     public static void Save(string id, Bounds bounds)
+        => GetPath(id).WriteAllTextToFilePath(bounds.Serialize(Json.Defaults));
+
+    static string GetPath(string id)
         => Environment
             .GetFolderPath(Environment.SpecialFolder.ApplicationData)
             .AppendPath(id)
             .SideEffect(d => d.EnsureDirectoryExists())
-            .AppendPath("bounds.json")
-            .WriteAllTextToFilePath(bounds.Serialize(Json.Defaults));
+            .AppendPath("bounds.json");
 }
